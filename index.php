fd27723759d8b2b3d2566cdfdb6294160e04a75d
Hafida3412 <lounsy81@gmail.com>
2024-08-30 14:25:56 +0200

    creation du styles.css

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href= "/css/styles.css">
 <div class="carousel">
   <div class="carousel__item carousel__item--left">
     <img src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="dog">
